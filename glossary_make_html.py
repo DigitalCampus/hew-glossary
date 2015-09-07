29af49ddf0ff34e47ac0f53ac3683d9381682053
alexlittle <alex@alexlittle.net>
2015-09-07 14:31:47 +0300

    Updated glossary

--- glossary_make_html.py
+++ glossary_make_html.py
@@ -11,10 +11,10 @@ INFILE = os.path.join(BASE_DIR, 'input', 'glossary.csv')
 OUTPUT_DIR = os.path.join(BASE_DIR, 'output')
 
 CSV_FORMAT = {
-              'term_en': 0,
-              'desc_en': 1,
-              'term_ti': 2,
-              'desc_ti': 3,
+              'term_en': 1,
+              'desc_en': 2,
+              'term_ti': 3,
+              'desc_ti': 4,
               'credit_ti': 4,
               'term_am': 5,
               'desc_am': 6,
@@ -28,64 +28,47 @@ def run():
     alphabet_used = []
     f = None
     
+    pages = {}
+    
     with open(INFILE, 'rb') as csvfile:
         file_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        
         for counter, row in enumerate(file_reader):
             # skip first row as has the headings
             if counter == 0:
                 continue
+            term_en = row[CSV_FORMAT['term_en']][0].lower()
+             
+            if term_en in pages:
+                pages[term_en] = pages[term_en] + 1
+            else:
+                pages[term_en] = 1
+            
+            print pages
             
             term_en = row[CSV_FORMAT['term_en']]
-            print term_en
             if term_en[0].lower() != current_alpha:
                 if f and current_alpha:
-                    f.write("</table>\n</body></html>")
+                    f.write("</body></html>")
                     f.close()
                 current_alpha = term_en[0].lower()
                 out_file = os.path.join(OUTPUT_DIR, current_alpha + ".html")  
                 f = open(out_file, 'w') 
                 f.write("<html>\n<head>")
-                f.write ("<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" /></head>\n<body>\n<table>\n")
-                f.write("<tr><th>Term</th><th>Term (lang)</th><th>Definition</th></tr>\n")
-    
-            f.write("<tr>")
-            f.write("<td>")
-            f.write(term_en)
-            f.write("</td>")
-            f.write("<td>")
-            f.write(term_en)
-            f.write("</td>")
-            f.write("<td>")
-            f.write(row[CSV_FORMAT['desc_en']])
-            f.write("</td>")
-            f.write("</tr>\n")
+                f.write ("<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" /></head>\n<body>\n")
             
-
+            f.write("\n<h2>")
+            f.write(term_en)
             if row[CSV_FORMAT['term_ti']]:
-                f.write("<tr>")
-                f.write("<td>")
-                f.write("&nbsp;")
-                f.write("</td>")
-                f.write("<td>")
-                f.write(row[CSV_FORMAT['term_ti']])
-                f.write("</td>")
-                f.write("<td>")
+                f.write(" - " + row[CSV_FORMAT['term_ti']])
+            f.write("</h2>\n")
+            f.write("<p>")
+            f.write(row[CSV_FORMAT['desc_en']])
+            f.write("</p>\n")
+            if row[CSV_FORMAT['desc_ti']]:
+                f.write("<p>")
                 f.write(row[CSV_FORMAT['desc_ti']])
-                f.write("</td>")
-                f.write("</tr>\n")
-            
-            if row[CSV_FORMAT['term_am']]:
-                f.write("<tr>")
-                f.write("<td>")
-                f.write("&nbsp;")
-                f.write("</td>")
-                f.write("<td>")
-                f.write(row[CSV_FORMAT['term_am']])
-                f.write("</td>")
-                f.write("<td>")
-                f.write(row[CSV_FORMAT['desc_am']])
-                f.write("</td>")
-                f.write("</tr>\n")
+                f.write("</p>\n")
             
     # create the index
     output_index = os.path.join(OUTPUT_DIR,"index.hml")    
